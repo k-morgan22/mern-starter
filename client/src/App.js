bf8c35e43efba4d4852ac4533621967a8878eede
kmorgan <kiki.morgan13@gmail.com>
2020-11-20 21:23:52 -0500

    mern starter files

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, NavLink, Switch} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">     
+      <Router>
+        <h1>Start Coding in your local IDE, no setup necessary!!</h1>
+      </Router>
+    </div>
+  );
+}
+
+
+export default App;
