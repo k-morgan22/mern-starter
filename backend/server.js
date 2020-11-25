9ba1a60d5e9c9a38fdf303749944a7623ea5927d
kmorgan <kiki.morgan13@gmail.com>
2020-11-25 16:17:00 -0500

    example app that works with vscode remote

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
-
 const express = require('express'); 
 const mongoose = require('mongoose');
+// const cors = require('cors');
 const router = require('./routes/index');
 
 const app = express(); 
 const PORT = 3001; 
 const MONGODB_URI = "mongodb://app-db:27017/my_local_db"; 
 
+// app.use(cors())
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 app.use('/api', router); 
