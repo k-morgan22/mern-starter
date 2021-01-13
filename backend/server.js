81a357222317cd3ba19f493f88d2ff282fb5c379
kmorgan <kiki.morgan13@gmail.com>
2021-01-13 03:24:10 -0500

    add config options, docker or local

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,19 @@ const mongoose = require('mongoose');
 // const cors = require('cors');
 const router = require('./routes/index');
 
+process.env.NODE_CONFIG_DIR = "../config/"
+const config = require("config");
+
 const app = express(); 
 const PORT = 3001; 
-const MONGODB_URI = "mongodb://app-db:27017/my_local_db"; 
+const MONGODB_URI = config.get('db');
 
 // app.use(cors())
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json());
 app.use('/api', router); 
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false }); 
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }); 
 mongoose.connection.once('open', function() { 
   console.log('Connected to the Database.');
 });
